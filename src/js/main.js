477a141f5c1b5764f0cf0d9828d983adcaf89dfc
Dustin Fadler <dfadler@paradowski.com>
2013-10-27 19:23:33 -0500

    Adds a 100ms timeout to keep the calculation from firing everytime

--- src/js/main.js
+++ src/js/main.js
@@ -7,14 +7,14 @@ require(['config'], function() {
         'polyfill/map',
         'polyfill/filter',
         'polyfill/console-log'
-        ], function() {
+    ], function() {
 
         require([
             'text!root/data.json',
             'helpers/sort',
             'helpers/array'
         ], function(data, sort, arrayUtils) {
-            
+
             data = JSON.parse(data);
 
             var img = document.getElementById('image-12345'),
@@ -24,32 +24,43 @@ require(['config'], function() {
                 imageData;
 
             function getSortedWidths() {
-                
+
                 var widths = data.map(function(dataSegment) {
-                        return dataSegment.width;
-                    });
+                    return dataSegment.width;
+                });
 
                 return widths.sort(sort.numericallyAscending);
 
             }
 
+            var timeout;
+
             function setImageSrc(e) {
-                
-                windowWidth = window.outerWidth || document.body.clientWidth;
 
-                widths = getSortedWidths();
-                
-                closest = arrayUtils.findClosest(widths, windowWidth);
-
-                imageData = data.filter(function(dataSegment) {
-                    if(dataSegment.width === closest) {
-                        return dataSegment;
-                    }
-                })[0];
-
-                if(img.src !== imageData.url) {
-                    img.src = imageData.url;
+                if (timeout) {
+                    clearTimeout(timeout);
+                    timeout = false;
                 }
+
+                timeout = setTimeout(function() {
+
+                    windowWidth = window.outerWidth || document.body.clientWidth;
+
+                    widths = getSortedWidths();
+
+                    closest = arrayUtils.findClosest(widths, windowWidth);
+
+                    imageData = data.filter(function(dataSegment) {
+                        if (dataSegment.width === closest) {
+                            return dataSegment;
+                        }
+                    })[0];
+
+                    if (img.src !== imageData.url) {
+                        img.src = imageData.url;
+                    }
+
+                }, 100);
 
             }
 
